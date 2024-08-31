f224aad1d83b99026a90c2ff8325cf3d52ee40ca
MatherAdil <adilmather24@gmail.com>
2024-08-31 23:44:05 +0500

    First commit for Python data structure

---
+++ class-1-A5-GlobalVariables.py
@@ -0,0 +1,18 @@
+# Global Variables
+
+g = "language"
+
+def myfunc():
+  print("Python is " + g)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x) 
